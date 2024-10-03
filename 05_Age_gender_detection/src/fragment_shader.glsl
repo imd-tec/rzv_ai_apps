9b483e1c6a0d484a9019fbbc6d255e7e7afd9bf0
William Bright <William.Bright@imd-tec.com>
2024-10-03 13:32:43 +0100

    Colour swap is working

--- 05_Age_gender_detection/src/fragment_shader.glsl
+++ 05_Age_gender_detection/src/fragment_shader.glsl
@@ -7,5 +7,7 @@ varying vec2 Frag_UV;
 varying vec4 Frag_Color;
 void main()
 {
-    gl_FragColor = Frag_Color * texture2D(Texture, Frag_UV.st);
+    vec4 texColor = texture2D(Texture, Frag_UV.st);
+    gl_FragColor = vec4(texColor.b, texColor.g, texColor.r, texColor.a);
+    //gl_FragColor = Frag_Color * texture2D(Texture, Frag_UV.st);
 }
