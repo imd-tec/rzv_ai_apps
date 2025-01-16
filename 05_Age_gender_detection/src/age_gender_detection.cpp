8d05b4f24786d5f7990ca07056ae78008db304f4
William Bright <William.Bright@imd-tec.com>
2025-01-16 15:55:49 +0000

    Changed for the 1024x600 screen

--- 05_Age_gender_detection/src/age_gender_detection.cpp
+++ 05_Age_gender_detection/src/age_gender_detection.cpp
@@ -1602,7 +1602,7 @@ int main(int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_FULLSCREEN);
-    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, window_flags);
+    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 600, window_flags);
     if (window == nullptr)
     {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
@@ -1669,7 +1669,7 @@ int main(int argc, char *argv[])
 #else
 
 #endif
-    OCA_Activate( &OCA_list[0] );
+    //OCA_Activate( &OCA_list[0] );
 
     drpai_freq = DRPAI_FREQ;
 
